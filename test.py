41254216b906d9b1e00f14e263a143028decbc48
Samuel Atkins <sjatkins12@gmail.com>
2018-08-29 15:51:01 -0700

    ngrok to connect localhost to slack bot

--- test.py
+++ test.py
@@ -9,11 +9,15 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 @app.route("/process", methods=['POST'])
 def ask():
+    print (request)
     question = ''
     if 'text' in request.form:
         question = request.form['text']
     elif request.json and 'question' in request.json:
         question = request.json['question']
+    elif request.json and 'challenge' in request.json:
+        print("We have been challenged")
+        return request.json['challenge']
     else:
         abort(400)
 
@@ -21,7 +25,7 @@ def ask():
     # Entry Point For Quesiton to NLP
     # ret = find_answers(question)
     ret = {
-        'electedAnswer': "Cause He dislikes Tom Brady",
+        'electedAnswer': "Cause he dislikes Tom Brady",
         'buckets': [32, 56, 12],
         'bestAnswers': [453, 543, 687]
     }
